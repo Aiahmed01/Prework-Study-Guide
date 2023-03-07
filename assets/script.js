eabee00e54c4c41066fe2aa65f76e4a182ab9c28
Abdulla <default@defaults-iMac.local>
2023-03-07 00:13:35 -0600

    JS code updated

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+ console.log(topics[x]);
+}
+
 /*
 if (topic === 'HTML') {
  console.log("Let's study HTML!");
@@ -11,9 +17,3 @@ if (topic === 'HTML') {
  console.log('Please try again!');
 }
 */
-
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for (var x = 0; x < topics.length; x++ )
-{
-    console.log(topics[x]);
-}
